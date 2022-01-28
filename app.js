e9e72b11da798e27f6d70fcc895d6b25caed6d75
anishro <anishmanuudr@gmail.com>
2022-01-28 22:53:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const bodyParser = require("body-parser");
+const https = require("https");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  var url = "https://dad-jokes.p.rapidapi.com/random/joke";
+  const options = {
+    method: "GET",
+    hostname: "dad-jokes.p.rapidapi.com",
+    port: null,
+    path: "/random/joke",
+    url: url,
+    headers: {
+      "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+      "x-rapidapi-key": "204acc9554msh8836f5045b00346p1aa394jsn88f651765729",
+      useQueryString: true,
+    },
+  };
+  const request = https.request(options, function (response) {
+    const chunks = [];
+    res.setHeader("Content-Type", "text/html");
+    response.on("data", function (data) {
+      const joke = JSON.parse(data);
+      const setup = joke.body[0].setup;
+      const punchline = joke.body[0].punchline;
+      res.write(setup + "<br/>");
+      res.write(punchline);
+      res.send();
+    });
+
+    response.on("end", function () {
+      const body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+  request.end();
+});
+
+app.listen(3000, function () {
+  console.log("server is live at localhost:3000");
+});
